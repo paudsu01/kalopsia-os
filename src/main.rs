8e05e6b57dda80812339895abef4bcc353944c45
Supreme Paudel <supremepaudel49@gmail.com>
2025-09-28 11:30:19 -0500

    Setup rust bare bones

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// Custom panic handler since std lib is disabled
+#[panic_handler]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/** `_start` function
+ * Custom entry point to overwite default rust's crt0 entry point
+ * Disabled name mangling to help the linker.
+ * use extern "C" so that the compiler uses C calling convention for this function
+ */
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
