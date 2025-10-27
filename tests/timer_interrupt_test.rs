c53cc2efad3dac944aa2dd59220fa75b868c24e8
Supreme Paudel <supremepaudel49@gmail.com>
2025-10-27 17:23:28 -0500

    Add test for timer interrupts

--- tests/timer_interrupt_test.rs
+++ tests/timer_interrupt_test.rs
@@ -5,7 +5,6 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
-use kalopsia_os::serial_print;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
@@ -17,16 +16,20 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(kalopsia_os::gdt::DOUBLE_FAULT_STACK_INDEX as u16);
         }
+        idt[kalopsia_os::interrupts::Interrupts::Timer as u8]
+            .set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    kalopsia_os::serial_print!("Running tests/timer_interrupt_test.rs");
     kalopsia_os::gdt::init();
     IDT.load();
 
-    test_main();
+    kalopsia_os::interrupts::init_pics();
+    kalopsia_os::interrupts::enable();
     loop {}
 }
 
@@ -35,20 +38,11 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     kalopsia_os::panic_handler(info);
 }
 
-// Need to check if stack overflow case is handled properly by switching the stack while handling the double fault handler
-#[test_case]
-fn test_stack_overflow() {
-    stack_overflow();
-}
-
-#[allow(unconditional_recursion)]
-fn stack_overflow() {
-    stack_overflow();
-    serial_print!("Should not reach here");
-}
-
 extern "x86-interrupt" fn double_fault_handler(_frame: InterruptStackFrame, _error_code: u64) -> ! {
-    kalopsia_os::serial_println!(" ..[ok]\n");
+    panic!();
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_frame: InterruptStackFrame) {
+    kalopsia_os::serial_println!(" ..[ok]");
     kalopsia_os::exit_qemu(kalopsia_os::QEMUExitCode::Success);
-    loop {}
 }
